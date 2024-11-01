9ebcd9ca6c174b7d5f8f2c19a6579585df4f64f1
akanepajs <40496475+akanepajs@users.noreply.github.com>
2024-11-02 01:53:25 +0200

    Update anai_inspect_task_v2.py

--- anai_inspect_task_v2.py
+++ anai_inspect_task_v2.py
@@ -4,21 +4,6 @@ from inspect_ai.dataset import json_dataset, Sample
 from inspect_ai.solver import multiple_choice
 from inspect_ai.scorer import choice
 
-@task
-def anai() -> Task:
-    dataset = json_dataset(
-        'anai/artifacts/curated.json',
-        sample_fields=record_to_sample,
-    )
-
-    return Task(
-        dataset=dataset,
-        solver=multiple_choice(),
-        scorer=choice(),
-    )
-if __name__ == "__main__":
-    anai()
-
 def record_to_sample(record: dict[str, Any]) -> Sample:
     return Sample(
         input=record['scenario'] + ' ' + record['question'],
@@ -28,3 +13,19 @@ def record_to_sample(record: dict[str, Any]) -> Sample:
         ],
         target="A" if record['correct_option'] == 'option_a' else "B",
     )
+
+@task
+def anai() -> Task:
+    dataset = json_dataset(
+        'artifacts/curated.json',
+        sample_fields=record_to_sample,
+    )
+
+    return Task(
+        dataset=dataset,
+        solver=multiple_choice(),
+        scorer=choice(),
+    )
+
+if __name__ == "__main__":
+    anai()
